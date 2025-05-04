fa5c46a25cb240941714127a2f3267e2233efc35
rafaelofil <rafaelofil@outlook.com>
2025-05-04 20:11:06 -0300

    install npm

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,4 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   base: "./",
   plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
 });
